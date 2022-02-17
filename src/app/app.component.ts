79994f60b93b7270da41287bc000c5512e1a4f35
eunjune <iws1605@naver.com>
2022-02-17 21:49:13 +0900

    @Component의 실체

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 /*
 * 클래스를 컴포넌트로 만듬
+*
+* function Component(constructorFn?: Function) {
+*   // 클래스를 꾸며주는 로직들
+*   return constructorFn // 클래스의 constructor
+* }
 * */
 @Component({
   selector: 'app-root', // 컴포넌트 태그 네임
